e71c574dc6682f871d81ebef4652889ae31bba21
Dio <mkiemasimandio@gmail.com>
2025-04-28 17:37:53 +0700

    Initial commit - React crash course todo project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = () => {
+    if (input) {
+      setTodos([...todos, input]);
+      setInput("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "20px" }}>
+      <h1>Todo App 📝</h1>
+      <input value={input} onChange={(e) => setInput(e.target.value)} placeholder="Add Todo" />
+      <button style={{ marginLeft: "10px" }} onClick={addTodo}>
+        Add
+      </button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
